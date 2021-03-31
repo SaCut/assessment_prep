278224b962cfaa5acfe2ad6a87ccd7b314a1eaac
Saverio <SaCut@users.noreply.github.com>
2021-03-31 12:15:58 +0100

    further prep

--- assessment_oleg.py
+++ assessment_oleg.py
@@ -1,1 +1,13 @@
-assessment_oleg.py
+# declare a function that takes two arguments as integers
+# divide the first value by the second value
+# return the outcome
+# check if the number given is divisible by zero
+# throw an error if it can't, else the value
+
+def division(num1, num2):
+	try:
+		return num1 / num2
+	except Exception as e:
+		return e
+
+print(division(20, 5))
