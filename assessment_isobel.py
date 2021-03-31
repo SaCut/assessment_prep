f47ab8affa1c8455df4164dd44bedee6513b0881
Saverio <SaCut@users.noreply.github.com>
2021-03-31 11:04:07 +0100

    further prep

---
+++ assessment_isobel.py
@@ -0,0 +1,9 @@
+# create a class called Isobel and initialise the class to take two arguments
+# create an object of that class
+
+class Isobel():
+	def __init__(self, name, date):
+		self.name = name
+		self.date = date
+
+instance = Isobel("Isobel", "6/4/1998")
