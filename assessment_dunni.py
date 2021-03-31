aee850bc2643490600d5a99053e662fcaaabec98
Saverio <SaCut@users.noreply.github.com>
2021-03-31 11:18:40 +0100

    further prep

--- assessment_dunni.py
+++ assessment_dunni.py
@@ -1,1 +1,5 @@
-# 
+# cerate a method that takes one argument as a string (that string is your name)
+# and if the name == "dunni" returns true, else false
+
+def checkname(name):
+	return name == "dunni"
